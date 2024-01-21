20e00b63460d7222444dc0ed20b0ffc12c777cbe
Maxime Fournier <maxime.fournier@collegemv.qc.ca>
2024-01-21 11:46:54 -0500

    Create rencontre2.java

---
+++ rencontre2.java
@@ -0,0 +1,152 @@
+package poo;
+public class rencontre2 {
+
+    public static void afficheResult() {
+        //                        COURS 1.1
+        System.out.println("#################                      Types primitifs                   ###################");
+        System.out.println("\n");
+
+        // Créer et afficher les variables avec le meilleur type possible
+        // Variable contenant l'age d'une personne
+        // Population du cégep Marie-Victorin
+        // Variable comptant la population au Qc
+        // Variable pour calculer le revenue d'un individu
+        // Nombre de gouttes d'eau dans une piscine.
+        // Revenu d'Elon Musk
+        // Une variable qui se rappelle si le prof était bon ou pas
+        // Note en lettre que vous voulez à ce cours
+
+
+
+
+        System.out.println("\n#########################################################################################");
+
+        //                        COURS 1.2
+        System.out.println("\n#################                      OPÉRATEURS                      ###################");
+        System.out.println("#### Arithmétique ####");
+        int i = 9;
+        // Additionne 50 et 34
+
+        // Soustrait 50 à 34
+
+        // Mutiplie 4 par 80
+
+        // Divise 80 par 4
+
+        // 50 modulo 3
+
+        // Ajoute 1 à la variable i
+
+
+        // Retire 1 à la variable i
+
+
+        System.out.println("#### Opérateurs d'assignation ####");
+        int demo = 0;
+        // Assigne 8 à demo
+
+
+        // Augmente la valeur de  demo de 100
+
+
+        // Reduit la valeur de demo de 46
+
+
+        // Multiplie la valeur actuel de demo par 5
+
+
+        // Divise la valeur actuel de demo par 2
+
+
+        // Assigne a demo le modulo par 4 de sa valeur actuelle
+
+
+
+        System.out.println("#### Opérateurs comparaison et logique ####");
+        // Enregistre dans une variable si 44 est égal à 66
+
+        // Enregistre dans une varible si 44 n'est pas égal a 66
+
+        // Enregistre dans une varible si 44 est plus grand que 66
+
+        // Enregistre dans une varible si 44 est plus petit ou égal à 66
+
+
+
+
+        boolean isHealthy = true;
+        boolean isCheap = false;
+        // Enregistre dans une variable si les variable isHealthy et isCheap sont toutes les 2 vraies
+
+
+        // Enregistre dans une variable si les variable isHealthy est fausse et isCheap est vrai
+
+
+
+        // Enregistrer dans une varible si isHealthy ou isCheap est vrai
+
+
+        //                        COURS 1.3
+        System.out.println("#### STRINGS ####");
+        // Enregistre l'alphabet en minuscule dans une variable
+
+
+        // Affiche la longeur de la varibale alphabet
+
+
+        // Affiche l'alphabet en majuscule
+
+
+
+        // Trouve la position du f dans l'alphabet
+
+
+
+        // Créez deux variables nommées prénom et nom. Assignez-leur vos propres renseignements.
+        // Afficher prenom nom de tel sort que pour moi, il s'afficherait Maxime Fournier
+
+
+
+
+        // Écrire ceci avec un seul System.out.println:
+        // L'apostrophe peut briser le code.
+        // Les guillemets "Sont dangereux".
+
+
+        System.out.println("\n#### Contrôle ####");
+
+        // Créez les varibales int1, int2 et int3 contenant 3 entiers
+        // Sans supposé que vous savez la valeur de ces entiers, sauvegarder la valeur du plus grand des nombres dans une variable nommée plusGrand
+
+
+
+
+
+
+
+
+        // Si le plus gros était int1 afficher : Le plus gros est int1
+        // Si le plus gros était int2 afficher : Le plus gros est int2
+        // Si le plus gros était int3 afficher : Le plus gros est int3
+        // N'utilisez pas plus de 1 if toujours en supposant ne pas savoir lequel est le plus gros, mais vous pouvez utiliser plusGrand
+
+
+
+
+
+
+
+
+
+        // Même chose que le dernier exercice, mais sans if cette fois.(switch)
+
+
+
+
+
+        // Si age plus grand ou égal à 18 retourne majeur, sinon mineur sans if ni switch
+
+        
+
+    }
+}
