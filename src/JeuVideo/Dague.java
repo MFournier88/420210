1cfc3ff1b9b3164690c9364b7ee32f1d80e90f12
MFournier88 <maxime.fournier@collegemv.qc.ca>
2024-03-27 10:28:45 -0400

    Start

---
+++ src/JeuVideo/Dague.java
@@ -0,0 +1,9 @@
+package JeuVideo;
+
+public class Dague extends Arme{
+
+    public Dague(short dpc) {
+        super(dpc);
+        this.nom = "Dague";
+    }
+}
---
+++ src/Save/Arbalete.java
@@ -0,0 +1,9 @@
+package Save;
+
+public class Arbalete extends Arme {
+    public Arbalete(short dpc) {
+        super(dpc);
+        this.nom = "Arbalete";
+
+    }
+}
