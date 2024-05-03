60422c586baa7e2051fbabfddbe83479a58058cc
la-sarita <sara.boumehraz@collegemv.qc.ca>
2024-05-03 09:05:26 -0400

    Quelques corrections dans Tuile.

--- Code_vu_en_classe/jeuEchec/src/main/java/ca/mv/projet/composants/Tuile.java
+++ Code_vu_en_classe/jeuEchec/src/main/java/ca/mv/projet/composants/Tuile.java
@@ -3,6 +3,7 @@ package ca.mv.projet.composants;
 import ca.mv.projet.models.Echiquier;
 import ca.mv.projet.models.cases.Position;
 import javafx.fxml.FXML;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
@@ -23,7 +24,7 @@ public class Tuile extends StackPane {
         ajouterEvenementsTuile();
     }
 
-    public Tuile(int i, int j, Echiquier echiquier) {
+    public Tuile(int ligne, int colonne, Echiquier echiquier) {
         // TODO: Compléter le code manquant
         // appeler le constructeur à un paramètre
         // initialiser la attributs d'instance
@@ -38,38 +39,37 @@ public class Tuile extends StackPane {
         return null;
     }
 
-    public void ajouterEvenementsTuile(){
-        setOnDragOver(event -> {
-            ajouterEvenementsTuileFinDrag();
+    public void ajouterEvenementsTuile() {
+        this.setOnDragOver(event -> {
+            ajouterEvenementsTuileFinDrag(event);
         });
 
         // TODO: ajouter la gestion des evènements manquants
         // en lien avec le drag-and-drop
     }
 
-    public Tuile getTuileParPosition(Position destPos){
+    public Tuile getTuileParPosition(Position destPos) {
         GridPane parent = (GridPane) (this.getParent());
 
         return (Tuile) parent.getChildren().get(destPos.convertirEnIndex());
     }
 
-    public void ajouterEvenementsTuileFinDrag(){
-        setOnDragOver(event -> {
-            if (event.getGestureSource() != this && event.getDragboard().hasImage()) {
-                if(this.getChildren().size() == 2){
-                    image = (PieceImage) this.getChildren().get(1);
-                }
-
-                // TODO: ajouter le code approprié si nécessaire
-                event.acceptTransferModes(TransferMode.MOVE);
+    public void ajouterEvenementsTuileFinDrag(DragEvent event) {
+        if (event.getGestureSource() != this && event.getDragboard().hasImage()) {
+            if (this.getChildren().size() == 2) {
+                image = (PieceImage) this.getChildren().get(1);
             }
-            event.consume();
-        });
+
+            // TODO: ajouter le code approprié si nécessaire
+            event.acceptTransferModes(TransferMode.MOVE);
+        }
+        event.consume();
+
     }
 
     // TODO : Complétez le code si vous allez utiliser cet evenement sinon remplacer le par le bon
-    public void ajouterEvenementsTuileDragDrop(){
-        setOnDragDropped(event -> {
+    public void ajouterEvenementsTuileDragDrop() {
+        this.setOnDragDropped(event -> {
             Dragboard db = event.getDragboard();
             if (db.hasImage()) {
                 // Logique pour vérifier le mouvement valide
